0b4247699f5e8876f73c0a1bfa8550ed8a485700
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-03 13:14:16 +0200

    Greeting component that will display a greeting

---
+++ src/features/greetings/Greeting.jsx
@@ -0,0 +1,25 @@
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import style from './greeting.module.css';
+import { greetingsRequest } from './greetingReducer';
+
+export default function Greeting() {
+  const dispatch = useDispatch();
+  const { greeting, loading, error } = useSelector((state) => state.greeting);
+  useEffect(() => {
+    dispatch(greetingsRequest());
+  }, [dispatch]);
+
+  const handler = () => {
+    dispatch(greetingsRequest());
+  };
+
+  return (
+    <div className={style.container}>
+      {loading && <div className={style.loader} />}
+      {error && <p>Error!</p>}
+      <h1>{!loading && greeting}</h1>
+      {!loading && <button type="button" className={style.button1} onClick={handler}>Refresh</button>}
+    </div>
+  );
+}
