7924d9b4d76786e3e21c88ef91a544e864b8906e
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-03 13:19:11 +0200

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Greeting from './features/greetings/Greeting.jsx';
+import Greeting from './features/greetings/Greeting';
 import store from './features/greetings/greetingStore';
 
 function App() {
